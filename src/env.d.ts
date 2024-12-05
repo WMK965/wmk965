c9eac4c2daba9523f0aadc32e4db913e1ceb1a6d
WMK965 <wmk153024@gmail.com>
2024-12-05 14:36:29 +0800

    我是傻逼

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
-declare namespace App {
-  interface Locals {
-    SITE_URL: string
-    RSS_URL: string
-    RSS_PREFIX: string
-  }
-}
